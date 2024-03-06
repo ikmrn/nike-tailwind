0e42702eab5b569923bedfe8254cab07d4e4da22
ikmrn <ibragimovkmrn@gmail.com>
2024-03-06 10:58:53 +0500

    feat: Create components

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  CustomerReviews,
+  SpecialOffer,
+  Subscribe,
+  Footer
+}
